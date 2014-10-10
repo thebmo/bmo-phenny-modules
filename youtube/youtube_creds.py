e2ba739ec75ab8a290cffddf16f3d3900da0bbc6
thebmo <bmosier@gmail.com>
2014-10-10 16:18:33 -0400

    adding the template credentials back

--- youtube/youtube_creds.py
+++ youtube/youtube_creds.py
@@ -1,5 +1,5 @@
 # Credentials for the youtube.py module
 
 class yCREDS:
-    DEV_KEY = 'AIzaSyCmp1cIUcfdKA_11oHEFuoEwhoJHN_SmHc'
-    CLIENT_ID = '1026226105500.apps.googleusercontent.com'
+    DEV_KEY = 'YOUR_DEV_KEY'
+    CLIENT_ID = 'YOUR_CLIENT_ID'
