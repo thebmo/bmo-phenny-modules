1e0535fd1af293b87e1ea16584cab4cd75bdf197
thebmo <bmosier@gmail.com>
2014-11-14 16:11:28 -0500

    made some grammar changes to doge

--- doge.py
+++ doge.py
@@ -1,26 +1,41 @@
 import random
+import nltk
+
 def doge(phenny, input):
     
-    doges = ['many', 'much', 'such', 'so', 'no']
-    d1 = random.choice(doges)
-    doges.remove(d1)
-    d2 = random.choice(doges)
-
     s = input.groups()[1]
+    
     if 'http' not in s and ':' not in s:
-        s = s.split(' ')
-        r1 = s[random.randrange(0, len(s))]
-        r2 = ''
-        while(r2 == '' or r2 == r1):
-            r2 = s[random.randrange(0, len(s))]
-        if r2[-2:] == 'er':
-            r2 = r2.strip('er')
-            r2 = ''.join((r2, 'ing'))
+        good_tags = ['NN', 'NNS', 'VBG', 'VBP']
+        doges = ['many', 'much', 'such', 'so', 'no']
+        words = []
+        tokes = nltk.pos_tag(nltk.word_tokenize(s))
+        
+        for word in tokes:
+            if word[1] in good_tags and len(word[0]) > 2 and '\'' not in word[0]:
+                words.append(str(word[0]))
+        if len(words) > 1:
+            # picks the 2 doge terms
+            d1 = random.choice(doges)
+            doges.remove(d1)
+            d2 = random.choice(doges)
 
-        phenny.say('wow')
-        phenny.say('                 %s %s' % (d1, r1))
-        phenny.say('    %s %s' % (d2, r2))
-doge.rule = r'^(.*?)(.+\b.+)$'
+            # picks 2 string words
+            s = []
+            s.append(random.choice(words))
+            words.remove(s[0])
+            s.append(random.choice(words))
+            
+            for i, word in enumerate(s):
+                if s[i][-2:] == 'er':
+                    s[i] = word.strip('er')
+                    s[i] = ''.join((s[i], 'ing'))
+
+            phenny.say('wow')
+            phenny.say('                 %s %s' % (d1, s[0]))
+            phenny.say('    %s %s' % (d2, s[1]))
+# doge.rule = r'^(.*?)(.+\b.+)$'
+doge.commands = ['doge']
     
 if __name__ == '__main__': 
    print __doc__.strip()
