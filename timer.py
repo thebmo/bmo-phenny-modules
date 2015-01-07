de660de3dbd143caa66900f778180e8ba4a2c058
thebmo <bmosier@gmail.com>
2015-01-07 16:08:47 -0500

    made timer take a target and time paramerters

--- timer.py
+++ timer.py
@@ -2,29 +2,42 @@ import time
 
 
 def meow(phenny, input):
-    if not input.admin: return
-    DELAY = int(input.groups()[1])
-    TIME = 3600*24
-    time.sleep(DELAY)
-    meow = [
-        '   _.---.._             _.---...__',
-        '.-\'   /\   \          .\'  /\     /',
-        '`.   (  )   \        /   (  )   /',
-        '  `.  \/   .\'\      /`.   \/  .\'',
-        '    ``---\'\'   )    (   ``---\'\'',
-        '            .\';.--.;`.',
-        '          .\' /_...._\ `.',
-        '        .\'   `.a  a.\'   `.',
-        '       (        \/        )',
-        '        `.___..-\'`-..___.\'',
-        '           \          /',
-        '            `-.____.-\'   meooooow!'
-        ]
+    if not input.admin:
+        phenny.msg(input.nick, 'uh uh uh. you didnt say the magic word')
+        return
+
+    try:
+        g = input.groups()[1]
+        g = g.split(' ')
+        TARGET = g[0]
+        DELAY = int(g[1])
     
-    while True:
-        for line in meow:
-            phenny.msg('lauren', line)
-        time.sleep(TIME)
+        TIME = 3600*24
+        time.sleep(DELAY)
+        meow = [
+            '   _.---.._             _.---...__',
+            '.-\'   /\   \          .\'  /\     /',
+            '`.   (  )   \        /   (  )   /',
+            '  `.  \/   .\'\      /`.   \/  .\'',
+            '    ``---\'\'   )    (   ``---\'\'',
+            '            .\';.--.;`.',
+            '          .\' /_...._\ `.',
+            '        .\'   `.a  a.\'   `.',
+            '       (        \/        )',
+            '        `.___..-\'`-..___.\'',
+            '           \          /',
+            '            `-.____.-\'   meooooow!'
+            ]
+        
+        while True:
+            for line in meow:
+                phenny.msg(TARGET, line)
+            time.sleep(TIME)
+
+    except:
+        USAGE = '.meow <TARGET> <SECONDS>'
+        phenny.say(USAGE)
+        pass
         
 meow.commands = ['meow']
 meow.priority = 'high'
